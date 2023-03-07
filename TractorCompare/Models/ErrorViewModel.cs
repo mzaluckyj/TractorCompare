f4eff742946cc039bfbe23c12aeb51db65659e77
Michael <m.zaluckyj@gmail.com>
2023-03-07 10:43:52 -0500

    Add project files.

---
+++ TractorCompare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TractorCompare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
