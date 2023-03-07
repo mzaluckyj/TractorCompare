2448eedbae93a6a6a3b6e19499bf8c0f1820f2d8
Michael <m.zaluckyj@gmail.com>
2023-03-07 10:53:28 -0500

    ingore json

---
+++ TractorCompare/Models/Tractors.cs
@@ -0,0 +1,24 @@
+﻿namespace TractorCompare.Models
+{
+    public class Tractors
+    {
+        public Tractors() 
+        {
+        }
+
+        public string tractorID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Class { get; set; }
+        public string HP { get; set; }
+        public string PTO { get; set; }
+        public string fuel { get; set; }
+        public string hydroSteer { get; set; }
+        public string hydroImp { get; set; }
+        public string threePT { get; set; }
+        public string loaderID { get; set; }
+
+        public string image { get; set; }
+
+    }
+}
